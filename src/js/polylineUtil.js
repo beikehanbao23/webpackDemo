1913887e3d57c5ce15144f7fbac8df2e296f2361
beikehanbao23 <369103724@qq.com>
2018-03-23 15:46:55 +0800

    add polyline decode test

--- src/js/polylineUtil.js
+++ src/js/polylineUtil.js
@@ -1,3 +1,12 @@
+var polyline = require('google-polyline');
+var str = polyline.encode([
+    [38.5, -120.2],
+    [40.7, -120.95],
+    [43.252, -126.453]
+]);
+console.log(str);
+console.log(polyline.decode(str));
+
 module.exports = function() {
     var polyline = require('google-polyline');
     var str = polyline.encode([

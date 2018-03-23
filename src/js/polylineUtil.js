b0b2a7a3e8ecb7e4c65d7d71a8f184f5b9754327
beikehanbao23 <369103724@qq.com>
2018-03-23 15:38:05 +0800

    Test polyline

---
+++ src/js/polylineUtil.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+    var polyline = require('google-polyline');
+    var str = polyline.encode([
+        [38.5, -120.2],
+        [40.7, -120.95],
+        [43.252, -126.453]
+    ]);
+    console.log(str);
+    return str;
+};

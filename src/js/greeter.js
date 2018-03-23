6f38484b7a2bf993d4a1b5809ebb2dd84f04c9db
beikehanbao23 <369103724@qq.com>
2018-03-23 09:03:52 +0800

    page import  js file

--- src/js/greeter.js
+++ src/js/greeter.js
@@ -2,5 +2,9 @@
 module.exports = function() {
     var greet = document.createElement('div');
     greet.textContent = "Hi there and greetings!";
+
+    greet = document.createElement('div');
+    greet.textContent = ('dic819ef447b3a5bb787ab');
+    console.log("f k u");
     return greet;
-  };
+}
